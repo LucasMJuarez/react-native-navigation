12cf1f5f94e733a91a16c5bc9285c63aa5f2ecd9
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 20:57:26 -0300

    add stack actions poptotop

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { globalStyles } from '../../themes/theme'
+import { PrimaryButton } from '../../components/shared/PrimaryButton'
+import { StackActions, useNavigation } from '@react-navigation/native'
 
 const SettingsScreen = () => {
+
+
+  const navigator = useNavigation()
   return (
-    <View>
-      <Text>
-        Welcome to the Settings Screen!
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom:20}}>
+        Settings Screen!
       </Text>
+      <PrimaryButton label='Regresar' onPress={() => navigator.goBack()} />
+      <PrimaryButton label='Regresar al Home' onPress={() => navigator.dispatch(StackActions.popToTop())} />
+
     </View>
   )
 }
