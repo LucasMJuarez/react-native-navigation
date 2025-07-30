1b69df925feae900496164d27f4d7473b32b91a0
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 14:00:51 -0300

    build project structure

---
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>
+        Welcome to the Settings Screen!
+      </Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
---
+++ src/presentation/themes/theme.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "#fff",
+    },
+    text: {
+        fontSize: 16,
+        color: "#333",
+    },
+});
+
+
+export const globalColors = {
+    primary: "#6200ee",
+    secondary: "#03dac6",
+    background: "#f5f5f5",
+}
