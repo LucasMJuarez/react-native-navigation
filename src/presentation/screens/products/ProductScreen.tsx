bd16843f50d8785fe44806f4ba2f0446b874e2d3
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 18:16:59 -0300

    pass params with use route in each products

--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,11 +1,29 @@
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { Text, View } from 'react-native'
+import { RootStackParam } from '../../routes/StackNavigator'
+import { globalStyles } from '../../themes/theme'
+import { useEffect } from 'react'
 
 const ProductScreen = () => {
+
+  const params = useRoute<RouteProp<RootStackParam, 'Product'>>().params
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: `Product ${params.id}`,
+    })
+      
+  }, [])
   return (
-    <View>
+    <View style={globalStyles.container}>
       <Text>
-        Welcome to the Product Screen!
+        Product Screen
       </Text>
+      <Text style={{ fontSize: 20, marginBottom: 20, textAlign: 'center' }}>
+        Product ID: {params.id} - {params.name} - ${params.price !== undefined ? params.price.toFixed(2) : 'N/A'}
+      </Text>
+
     </View>
   )
 }
