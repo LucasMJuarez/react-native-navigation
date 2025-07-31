5afbbf8e3112f768deac5dd8fa73a60573b84667
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 22:21:34 -0300

    toggle drawer in home

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,25 @@
-import {  View } from 'react-native';
+import {  Pressable, View } from 'react-native';
 import { globalStyles } from '../../themes/theme';
-import {type NavigationProp, useNavigation } from '@react-navigation/native';
+import {DrawerActions, type NavigationProp, useNavigation } from '@react-navigation/native';
 import { PrimaryButton } from '../../components/shared/PrimaryButton';
 import { type RootStackParam } from '../../routes/StackNavigator';
+import { useEffect } from 'react';
+import { Text } from 'react-native-gesture-handler';
 
 const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParam>>();
-
+ useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+          style={{ marginLeft: 10 }}
+        >
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
   return (
     <View style={globalStyles.container}>
       <PrimaryButton
