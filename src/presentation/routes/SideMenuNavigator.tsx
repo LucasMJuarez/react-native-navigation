c71aafd3f2db685865726d545ab18ee30161075b
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 22:38:37 -0300

    create drawer personalized

--- src/presentation/routes/SideMenuNavigator.tsx
+++ src/presentation/routes/SideMenuNavigator.tsx
@@ -1,14 +1,52 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import { StackNavigator } from './StackNavigator';
+import { globalColors } from '../themes/theme';
+import { useWindowDimensions, View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export function SideMenuNavigator() {
+
+    const dimensions = useWindowDimensions();
   return (
-    <Drawer.Navigator>
+    
+    <Drawer.Navigator
+        drawerContent={(props) => CustomDrawerContent(props)}// You can customize the drawer content here
+        screenOptions={{
+            drawerType: dimensions.width >= 768 ? 'permanent' : 'slide', // Use 'permanent' for larger screens
+
+          headerShown: false,
+          drawerStyle: {
+            width: 250,
+          },
+          drawerActiveBackgroundColor: globalColors.primary,
+          drawerActiveTintColor: 'white',
+          drawerInactiveTintColor:globalColors.primary
+        }}
+    >
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
     </Drawer.Navigator>
+  );
+}
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView {...props}>
+        <View
+            style= {{
+                height: 200,
+                backgroundColor: globalColors.primary,
+                margin:30,
+                borderRadius: 50,
+
+            }}
+
+        />
+
+       <DrawerItemList {...props} /* This will render the default drawer items */ />
+
+    </DrawerContentScrollView>
   );
 }
