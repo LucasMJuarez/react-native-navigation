9b11d8331aaa4b5e9310db444d21db02588f990a
lucas juarez <lujuarez@emeal.nttdata.com>
2025-07-30 22:50:45 -0300

    add usesafeareainserts

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>
-        Welcome to the Profile Screen!
-      </Text>
+    <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: top - 30 }}>
+      <Text style={{ marginBottom: 10 }}>Profile Screen!</Text>
+      <PrimaryButton
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+        label="Abrir Menu"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
